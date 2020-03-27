77b1aab46ce1c56bcc20cfb7eb560dc6478b21fc
Chris Ertel <>
2020-03-26 23:21:21 -0500

    Fix and cleanup temp_plugin.

--- ingest/user_data_sources/temp_plugin.py
+++ ingest/user_data_sources/temp_plugin.py
@@ -1,55 +1,44 @@
-from time import sleep
-import logging
+from datetime import date
+import time
 
+import csv
 from apscheduler.triggers.interval import IntervalTrigger
 
+from os.path import join, dirname, abspath
+
+import logging
 
 log = logging.getLogger(__name__)
 
+script_path = abspath(__file__)
+script_dir = dirname(script_path)
+
 
 def schedule():
-    """
-    Gets the scheduled run for the data source.
-    Returns the scheduled time for the data source.
-    """
-    # lolwut
     return IntervalTrigger(seconds=5)
 
 
 def init(runtime_id, name):
-    """
-    Intializes the data source.
-    Returns the initial state of the data source.
-    """
-
     return {}
 
 
 def get_fields():
-    # return {"date": "text", "high": "decimal", "low": "decimal"}
-    return [{"date": "string"}, {"high": "decimal"}, {"low": "decimal"}]
+    return {"date": "date", "high": "decimal", "low": "decimal"}
 
 
 def fetch_data(db_connection, run_id):
-    """
-    Fetches the data for a run.
-    Will insert data along with run into the DB.
-    """
-    tempTest = "datasets/sampleTemp.csv"
-    log.warning(f"pretending to fetch data for run {run_id}")
-    sleep(1)
-    x = 0
-    for row in tempTest:
-        return [{"date": row[0]}, {"high": row[1]}, {"low": row[2]}]
-        print(x)
-        x += 1
+    ret = []
+    csv_path = join(script_dir, "datasets/sampleTemp.csv")
+    with open(csv_path) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            pdate = time.strptime(row[0], "%d/%m/%Y")
+            row_date = date(year=pdate.tm_year, month=pdate.tm_mon, day=pdate.tm_mday)
+            row_high = float(row[1])
+            row_low = float(row[2])
+            ret.append({"date": row_date, "high": row_high, "low": row_low})
+    return ret
 
 
 def clean_data(db_connection, run_id, run_data):
-    """
-    Cleans up the data for a run.
-    Will insert the data from a run into the DB.
-    """
-    log.warning(f"pretending to clean data for run {run_id}")
-    sleep(1)
     return run_data
