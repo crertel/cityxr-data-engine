dfdd0733cf7e7fae3a9aab3deba32ea0f14d22ea
Morgan Montgomery <morgan.montgomery95@gmail.com>
2020-04-16 15:15:01 -0500

    Created heredoc webscrape template

---
+++ ingest/bin/create_web_plugin.sh
@@ -0,0 +1,39 @@
+cat << EOF > ingest/user_data_sources/webscrape_template.py
+from apscheduler.triggers.interval import IntervalTrigger
+import logging
+import requests
+from bs4 import BeautifulSoup
+
+log = logging.getLogger(__name__)
+
+
+def schedule():
+    return IntervalTrigger(""" INPUT TIME INTERVAL """)
+
+
+def init(runtime_id, name):
+    return {}
+
+
+def get_fields():
+    return {""" INPUT FIELDS + DATATYPES """}
+
+
+def fetch_data(db_connection, run_id):
+
+    page = requests.get(""" TARGET URL """)
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    items = []
+
+    table = soup.find(""" TARGET TAG """)
+
+    for row in table.find_all("TAG"):
+        """RECEIVE DATA"""
+        items.append({"x": DATA1, "y": DATA2, "z": DATA3})
+    return items
+
+
+def clean_data(db_connection, run_id, run_data):
+    return run_data
+EOF
