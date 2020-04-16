a1cf732ec1cef026d7bbdea5740066b8307a5c67
Morgan Montgomery <morgan.montgomery95@gmail.com>
2020-04-16 15:07:55 -0500

    Created heredoc commands to build blank or csv plugin templates

---
+++ ingest/bin/create_empty_plugin.sh
@@ -0,0 +1,41 @@
+cat << EOF > ingest/user_data_sources/empty_bin.py
+
+from apscheduler.triggers.interval import IntervalTrigger
+
+
+def schedule():
+    """
+    Gets the scheduled run for the data source.
+    Returns the scheduled time for the data source.
+    """
+    return IntervalTrigger(seconds=6)
+
+
+def init(runtime_id, name):
+    """
+    Intializes the data source.
+    Returns the initial state of the data source.
+    """
+    return {}
+
+
+def get_fields():
+    return {"a": "decimal"}
+
+
+def fetch_data(db_connection, run_id):
+    """
+    Fetches the data for a run.
+    Will insert data along with run into the DB.
+    """
+    return []
+
+
+def clean_data(db_connection, run_id, run_data):
+    """
+    Cleans up the data for a run.
+    Will insert the data from a run into the DB.
+    """
+    return run_data
+
+EOF
