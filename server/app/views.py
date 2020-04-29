b4fb56d6e9ceedb8b2ae2eccbebd451a644fb8d0
Chris Ertel <>
2020-04-29 01:48:51 -0500

    Teach server /datasources endpoint.

--- server/app/views.py
+++ server/app/views.py
@@ -1,11 +1,18 @@
-from app import app
-from flask import url_for, redirect, render_template, flash, g, session
+# from app import app
+# from flask import url_for, redirect, render_template, flash, g, session
 
 import logging
+from database_connection import DatabaseConnection
 
 log = logging.getLogger()
 
+db_connection = DatabaseConnection()
+db_connection.connect_to_database()
 
-@app.route("/heartbeat")
+
 def heartbeat():
     return ({"heartbeat": "ok"}, 200)
+
+
+def get_available_data_sources():
+    return (db_connection.get_available_datasources(), 200)
