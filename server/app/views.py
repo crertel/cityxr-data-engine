c6b3efa9fb64a08c0dcd6d0d7abac43f534214ae
Chris Ertel <>
2020-05-03 22:07:37 -0500

    Add properties for heartbeat route.

--- server/app/views.py
+++ server/app/views.py
@@ -2,6 +2,7 @@
 # from flask import url_for, redirect, render_template, flash, g, session
 
 import logging
+from datetime import datetime
 from database_connection import DatabaseConnection
 
 log = logging.getLogger()
@@ -9,9 +10,12 @@ log = logging.getLogger()
 db_connection = DatabaseConnection()
 db_connection.connect_to_database()
 
+boot_time = datetime.utcnow()
+
 
 def heartbeat():
-    return ({"heartbeat": "ok"}, 200)
+    now = datetime.utcnow()
+    return ({"bootTime": boot_time, "uptime": (now - boot_time).total_seconds()}, 200)
 
 
 def get_available_data_sources():
